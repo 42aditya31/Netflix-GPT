0b645377250e47f69317f3136d5436732989828e
Sharma Aditya <aditya.sharma4231.in@gmail.com>
2025-05-14 11:42:17 +0530

    Adding the structure for the project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-9xl font-bold text-pink-50'> Hello Ji </h1>
+    </div>
+  )
+}
+
+export default App
