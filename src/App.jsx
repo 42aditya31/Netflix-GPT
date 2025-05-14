81f3e3c11db14c24343b0c7d6c18be58a4142ab8
Sharma Aditya <aditya.sharma4231.in@gmail.com>
2025-05-14 18:22:26 +0530

    Created Redux-Store using userSlice

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Main from "./components/MainPage/Main";
+import {Provider} from "react-redux"
+import appStore from "./store/appStore";
 
 const App = () => {
   return (
     <div>
+      <Provider store={appStore}>
       <Main />
+      </Provider>
     </div>
   );
 };
