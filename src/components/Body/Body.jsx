3c5fe01019e7938a469656009c8e70b9d68d91fd
Sharma Aditya <aditya.sharma4231.in@gmail.com>
2025-05-15 16:57:11 +0530

    Creating the Slice to store the Now playing movies - Fetch the data from TMDB store into appStore

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import Header from '../Header/Header'
+import React, { useEffect } from "react";
+import Header from "../Header/Header";
+import { API_OPTIONS } from "../../utils/constant";
+import { useDispatch } from "react-redux";
+import { addnowPlayingMovies } from "../../store/movieSlice";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const getNowPlayingMovie = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    console.log(json.results);
+    dispatch(addnowPlayingMovies(json.results));
+  };
+  useEffect(() => {
+    getNowPlayingMovie();
+  }, []);
+
   return (
     <div>
-      <Header/>
+      <Header />
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,3 +3,12 @@ export const NETFLIX_LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-
 export const USER_PHOTO = "https://www.gstatic.com/mobilesdk/240501_mobilesdk/firebase_28dp.png";
 
 export const BACKGROUND_IMAGE ="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+
+
+export const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZjczNWQyYjdlNmFjNmU3Y2FlY2ZlOTI0NmJkYWU5YiIsIm5iZiI6MS43NDczMDM1MTUxOTM5OTk4ZSs5LCJzdWIiOiI2ODI1YmM1YjM3MWFhZDRjM2YyZDJhZGMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.bnmHXDt6YZJWO-Of-TXgONOAMgmU1SBrcCYKKq2aMsI'
+  }
+};
