06e415b5aaa63babf6db5d7202f2ac86dd25db2f
Sharma Aditya <aditya.sharma4231.in@gmail.com>
2025-05-20 15:46:48 +0530

    A complete Functionality to show the movies

--- src/components/GPTSearchFeture/GptMovieSuggestion.jsx
+++ src/components/GPTSearchFeture/GptMovieSuggestion.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import {useSelector} from 'react-redux'
+import MovieList from "../VideoListSection/MovieList"
 
 const GptMovieSuggestion = () => {
+
+  const {movieResults,moviesname } = useSelector((store)=>store.gpt)
+
+if (!moviesname) return null;
+
   return (
-    <div>
-      
+    <div className="p-4 m-4 bg-black text-white bg-opacity-90">
+      <div>
+        {moviesname.map((movieName, index) => (
+          <MovieList
+            key={movieName}
+            title={movieName}
+            movies={movieResults[index]}
+          />
+        ))}
+      </div>
     </div>
   )
 }
